a12f2a8b18c3b18d8b1547ad2f7371036c737a40
mligon115 <mligon115@yahoo.com>
2022-05-27 19:26:49 -0500

    updated table

--- index.php
+++ index.php
@@ -5,10 +5,6 @@
     $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
     $statement->execute();
     $products = $statement->fetchALL(PDO::FETCH_ASSOC);
-
-    echo '<pre>';
-    print_r($products);
-    echo '</pre>';
 ?>
 
 <!doctype html>
@@ -29,29 +25,27 @@
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Image</th>
+      <th scope="col">Title</th>
+      <th scope="col">Price</th>
+      <th scope="col">Create Date</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
+    <?php foreach($products as $i => $product): ?>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"><?php echo $i + 1 ?></th>
+      <td><?php echo $product['image'] ?></td>
+      <td><?php echo $product['title'] ?></td>
+      <td><?php echo $product['price'] ?></td>
+      <td><?php echo $product['create_date'] ?></td>
+      <td>
+          <button type="button" class="btn btn-primary">Edit</button>
+          <button type="button" class="btn btn-danger">Delete</button>
+      </td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php endforeach; ?>
   </tbody>
 </table>
 </body>
