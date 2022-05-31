f3fea88574b790914d46afb485335ca6138ab348
mligon115 <mligon115@yahoo.com>
2022-05-31 18:21:27 -0500

    first commit

--- create.php
+++ create.php
@@ -1,6 +1,5 @@
 <?php 
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    require_once "database.php";
 
     $errors = [];
 
@@ -59,52 +58,11 @@
     }
 
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link href="app.css" rel="stylesheet"/>
-    <title>Products CRUD</title>
-</head>
-<body>
+<?php include_once"views/partials/header.php"; ?>
   <h1>Create New Product</h1>
 
-
-  <?php if(!empty($errors)): ?>
-    <div class="alert alert-danger">
-      <?php foreach($errors as $error): ?>
-        <div><?php echo $error ?></div>
-      <?php endforeach; ?>
-    </div>
-  <?php endif; ?>
-
-<form action="" method="post" enctype="multipart/form-data">
-  <div class="form-group">
-    <label>Product Image</label>
-    <input type="file" name="image" class="form-control">
-  </div>
-  <div class="form-group">
-    <label>Product Title</label>
-    <input type="text" name="title" class="form-control" value=<?php echo $title?>>
-  </div>
-  <div class="form-group">
-    <label>Product Description</label>
-    <textarea name="description" class="form-control" value=<?php echo $description?>></textarea>
-  </div>
-  <div class="form-group">
-    <label>Product Price</label>
-    <input type="number" name="price" step=".01" class="form-control" value=<?php echo $price?>>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  <a href="index.php" class="btn btn-info">Go Back</a>
-</form>
+<?php require_once './views/partials/products/form.php'; ?>
+  
 
 </body>
 </html>
